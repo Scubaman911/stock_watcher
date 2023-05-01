12372c3fe3e4da3e432c6e1f41811ded12439432
CS <work@cjcsmith.com>
2023-05-01 21:45:39 +0100

    update functionality to be schedulable

--- stock_fetch.py
+++ stock_fetch.py
@@ -2,34 +2,44 @@ from typing import List
 from pprint import pprint
 from stock_builder import StockBuilder, Stock
 from stock_tickers import ticker_list
-from stock_utils import percentage_change
+from stock_utils import percentage_change, on_schedule
 from datetime import datetime
 import logging
 
-def main():
+@on_schedule
+def collate_stocks():
+    """
+    
+    """
     stocks: List[Stock] = []
     for stock in ticker_list:
         built = StockBuilder(stock)
         built.collate_daily_price_data()
         built.collate_news_data()
         stocks.append(built.stock)
+        logging.info(f"*** Info collated for {built.ticker}! ***")
+        # TO SEE STUFF
+        # logging.info(f"*** Info for {built_stock.ticker}! ***")
+        # logging.info("Daily Price Data:")
+        # pprint(built_stock.daily_price_data)
+        # pcnt = percentage_change(built_stock.daily_price_data.current_price, built_stock.daily_price_data.open_price)
+        # logging.info(f"PERCENTAGE CHANGE: {pcnt:.2f}%")
+        # logging.info("-----------------")
+        # logging.info("Stories:")
+        # for story in built_stock.news:
+        #    logging.info(story.title)
+        #    logging.info(story.publisher)
+        #    logging.info(datetime.fromtimestamp(story.published_timestamp))
+        #    logging.info(story.link)
+        #    logging.info("-----------------")
+        # logging.info("")
+        # logging.info("")
 
-    for built_stock in stocks:
-        logging.info(f"*** Info for {built_stock.ticker}! ***")
-        logging.info("Daily Price Data:")
-        pprint(built_stock.daily_price_data)
-        pcnt = percentage_change(built_stock.daily_price_data.current_price, built_stock.daily_price_data.open_price)
-        logging.info(f"PERCENTAGE CHANGE: {pcnt:.2f}%")
-        logging.info("-----------------")
-        logging.info("Stories:")
-        for story in built_stock.news:
-           logging.info(story.title)
-           logging.info(story.publisher)
-           logging.info(datetime.fromtimestamp(story.published_timestamp))
-           logging.info(story.link)
-           logging.info("-----------------")
-        logging.info("")
-        logging.info("")
+
+def main():
+    logging.info("Stock Watcher Started!")
+    collate_stocks()
+
 
 if __name__ == "__main__":
     logging.basicConfig(
--- stock_utils.py
+++ stock_utils.py
@@ -1,8 +1,36 @@
 """
 Data utilities for the stock data models.
 """
+import os
+import time
+import functools
+import logging
 
 def percentage_change(current_value: float, previous_value: float):
     x = (current_value - previous_value) / previous_value
     x_as_pcnt = x * 100
     return x_as_pcnt
+
+
+def call_on_schedule(func: callable):
+    secs = int(os.getenv("SCHEDULE_SECONDS", 60))
+    minutes = int(os.getenv("SCHEDULE_MINUTES", 0))
+
+    timer_secs = secs + (minutes * 60)
+    timer = timer_secs
+
+    while timer:        
+        time.sleep(1)
+        timer -= 1
+        if timer == 0:
+            logging.info(f"{func.__name__} is being called...")
+            func()
+            logging.info(f"{func.__name__} has finished, resetting timer...")
+            timer = timer_secs
+      
+def on_schedule(func):
+    @functools.wraps(func)
+    def wrapper():
+        logging.info(f"{func.__name__} has been submitted to be called on a schedule.")
+        call_on_schedule(func)
+    return wrapper
