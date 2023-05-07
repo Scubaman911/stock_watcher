db40910b0bf8a86106c0b52fc663ef9a26cc219b
CS <work@cjcsmith.com>
2023-05-07 18:45:16 +0100

    add features to publish to kafka topic

--- stock_fetch.py
+++ stock_fetch.py
@@ -1,38 +1,60 @@
-from typing import List
-from pprint import pprint
-from stock_builder import StockBuilder, Stock
-from stock_tickers import ticker_list
-from stock_utils import percentage_change, on_schedule
-from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor
 import logging
+from typing import List
+from concurrent.futures import ThreadPoolExecutor
+
+from confluent_kafka import Producer
+
+from stock_builder import StockBuilder, Stock
+from stock_utils import on_schedule
+from config import stock_config
+
 
 @on_schedule
-def collate_stocks():
+def publish_stocks():
     stocks: List[Stock] = []
     with ThreadPoolExecutor(max_workers=16) as executor:
-        for result in executor.map(build_stock, ticker_list):
+        for result in executor.map(build_stock, stock_config["tickers"]["list"]):
             stocks.append(result)
+            produce_stock_to_stream(result)
 
 
 def build_stock(stock):
     built = StockBuilder(stock)
     built.collate_daily_price_data()
     built.collate_news_data()
-    logging.info(f"*** Info collated for {built.ticker}! ***")
     return built.stock
 
+
+def on_delivery(err, msg):
+    if err:
+            logging.error('ERROR: Message failed delivery: {}'.format(err))
+    else:
+        logging.info("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+            topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
+
+def produce_stock_to_stream(stock: Stock):
+    kafka_config = stock_config["kafka"]
+    producer = Producer(kafka_config)
+
+    producer.produce(
+        topic=stock_config["topics"]["stock_producer_topic"],
+        value=str(stock.daily_price_data.current_price),
+        key=stock.ticker,
+        on_delivery=on_delivery,
+    )
+    producer.flush()
 
 
 def main():
     logging.info("Stock Watcher Started!")
-    collate_stocks()
+    publish_stocks()
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.INFO, 
-        format= '[%(asctime)s] %(levelname)s - %(message)s')
+        level=logging.INFO, format="[%(asctime)s] %(levelname)s - %(message)s"
+    )
     main()
 
 
